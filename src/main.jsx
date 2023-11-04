f3b94b99159dd94cb716742a17a3f6f3252a5b86
icobhavesh <bhaveshsaxena058@gmail.com>
2023-11-04 20:53:29 +0530

    change in  main.jsx Coin.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// import Home from './components/Home.jsx'
 
-// import Hero from './Coingeko/Hero.jsx'
+import './index.css'
+
 import Parents from './Crypto-Currency/Components/Parents.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App />
-    <Home /> */}
 
-    {/* <Hero /> */}
     <Parents/>
 
   </React.StrictMode>,
