e6fe512c76832e3d9a70b5b2de0eaa0ddf2a1bc1
icobhavesh <bhaveshsaxena058@gmail.com>
2023-11-06 14:58:27 +0530

    change in HistoryChart.jsx

--- src/Crypto-Currency/Components/HistoryChart.jsx
+++ src/Crypto-Currency/Components/HistoryChart.jsx
@@ -3,7 +3,7 @@ import { Line } from 'react-chartjs-2';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
-const HistoryChart = () => {
+const HistoryCharts = () => {
     const [chartData, setChartData] = useState({});
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
